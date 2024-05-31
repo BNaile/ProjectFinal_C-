474a41baf90cbefb544429c7aceb9d95cc7d279e
BNaile <bayramovanaile578@gmail.com>
2024-05-31 21:53:52 +0400

    new version

--- Entities/Concrete/Dtos/Team/TeamDto.cs
+++ Entities/Concrete/Dtos/Team/TeamDto.cs
@@ -19,6 +19,8 @@ namespace Entities.Concrete.Dtos
 
         public int PositionId { get; set; }
 
+        public string PositionName { get; set; }
+
         public static List<TeamDto> Toteam(Team team)
         {
             TeamDto dto = new TeamDto()
